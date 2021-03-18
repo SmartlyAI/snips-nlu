495b38c05cb3634868cbc016a706f4c848671788
Default user for cloud <cloud@smartly-rasa-preprod.novalocal>
2021-03-18 10:42:10 +0000

    update docker installation

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ class ApiConfigDev(object):
     EXECUTOR_TYPE = "thread"
     EXECUTOR_MAX_WORKERS = 5
     EXECUTOR_PROPAGATE_EXCEPTIONS = True
-    SSE_REDIS_URL = "redis://localhost:6379/0"
+    SSE_REDIS_URL = environ["SSE_REDIS_URL"]
     SECRET_KEY = "smartly-api-snips-nlu-2021-digital-data"
 
 class ApiConfigProd(ApiConfigDev):
     EXECUTOR_TYPE = "thread"
     EXECUTOR_MAX_WORKERS = 10
     EXECUTOR_PROPAGATE_EXCEPTIONS = True
-    SSE_REDIS_URL = "redis://localhost:6379/0"
+    SSE_REDIS_URL = environ["SSE_REDIS_URL"]
     SECRET_KEY = "smartly-api-snips-nlu-2021-digital-data"
