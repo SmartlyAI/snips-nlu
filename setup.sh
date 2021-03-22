7ddbced207da6d58b6ab7ffff98b243ac0d7fab1
ben-yacine <hoelath@gmail.com>
2021-03-22 11:27:14 +0000

    use https to clone repo from git

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ sudo mkdir /snips-data
 
 # Cloning mnemoose only if it does not exist
 if [ ! -d "snips-nlu-parse" ]; then
-  git clone git@github.com:SmartlyAI/snips-nlu-parse.git
+  git clone https://github.com/SmartlyAI/snips-nlu-parse.git
   cd snips-nlu-parse && git checkout $1 && cd ..
 else
   echo 'Updating snips-nlu-parse repository'
@@ -13,7 +13,7 @@ fi
 
 # Cloning atlas only if it does not exist
 if [ ! -d "snips-nlu-train" ]; then
- git clone git@github.com:SmartlyAI/snips-nlu-train.git
+ git clone https://github.com/SmartlyAI/snips-nlu-train.git
  cd snips-nlu-train && git checkout $1 && cd ..
 else
   echo 'Updating snips-nlu-train repository'
