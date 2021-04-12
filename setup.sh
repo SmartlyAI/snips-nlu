b14433320c942d453b3eb2b86bfee6c60ff2d726
YacineB <benabdesselam.yacine@gmail.com>
2021-04-12 14:55:03 +0200

    add rabbitmq and hestia

--- setup.sh
+++ setup.sh
@@ -1,3 +1,21 @@
+# Swarm alreay cerated with docker swarm init on azure
+docker swarm init
+
+# Create registry which will contains all images for smartly services
+docker service create --name registry-smartly-swarm --publish 5000:5000 --constraint 'node.role == manager' --detach=false registry:2
+
+# Create volumes
+docker volume create --name=rabbitmq
+
+# Cloning hestia only if it does not exist
+if [ ! -d "hestia" ]; then
+  git clone https://github.com/SmartlyAI/hestia.git
+  cd hestia && git checkout $1 && cd ..
+else
+  echo 'Updating hestia repository'
+  cd hestia &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
+fi
+
 # Cloning snips-nlu-parse only if it does not exist
 if [ ! -d "snips-nlu-parse" ]; then
   git clone https://github.com/SmartlyAI/snips-nlu-parse.git
@@ -6,8 +24,6 @@ else
   echo 'Updating snips-nlu-parse repository'
   cd snips-nlu-parse &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
 fi
-
-# SERVICES
 
 # Cloning snips-nlu-train only if it does not exist
 if [ ! -d "snips-nlu-train" ]; then
@@ -18,4 +34,5 @@ else
   cd snips-nlu-train &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
 fi
 
-docker-compose build && docker-compose up -d
+# Build, push on registry and create snips_nlu stack
+docker-compose build && docker-compose push && env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml snipsnlu
