77c745aeeb3ca27c74c11f2fd8d4bf02981e3278
YacineB <benabdesselam.yacine@gmail.com>
2021-05-19 19:13:08 +0200

    update repo  to work on premise

--- README.md
+++ README.md
@@ -4,11 +4,18 @@
 - Docker Version: 20.10.5
 - Docker-compose Version: 1.28.5
 
-## To deploy from scratch on production:
+# To deploy from scratch on production:
+
+## 1- Install/launch App stack:
 - First, you need to clone this repo with the command line `git clone`
 - Second, you need to add .env file which contains the environment variable used by docker-compose.
-- Third, launch the _setup.sh_ script with the following command `bash setup.sh magenta-develop`.
+- Third, launch the _setup.sh_ script with the following command `bash setup.sh`.
 
-## To install Nginx/Amplify:
-- launch the _nginx.setup.sh_ script with the following command `env $(cat .env | grep ^[A-Z] | xargs) sh nginx.setup.sh`.
+## 2- Configure Nginx:
+- launch the _nginx.setup.sh_ script with the following command `sh nginx.setup.sh`.
+
+# To update the web application:
+- First, pull the commits of the repo smartly-swarm-azure `git pull`.
+- If necessary: [Second, add new env variables in the file .env (Rarley)]
+- Finally, launch the stack `env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml snipsnlu`.
 
--- setup.sh
+++ setup.sh
@@ -4,55 +4,14 @@ sudo chown -R $USER:$USER /snips-data
 echo "/snips-data    ${DISTANT_SERVER_PRIVATE_IP}(rw,sync,no_subtree_check)" | sudo tee /etc/exports
 sudo systemctl restart nfs-kernel-server
 
-# Swarm alreay cerated with docker swarm init on azure
+# Init docker swarm
 docker swarm init
 
-# Create registry which will contains all images for smartly services
+# Create registry which will contains all images of smartly services
 docker service create --name registry-smartly-swarm --publish 5000:5000 --constraint 'node.role == manager' --detach=false registry:2
 
 # Create volumes
 docker volume create --name=rabbitmq
 
-# Cloning hestia only if it does not exist
-if [ ! -d "hestia" ]; then
-  git clone git@github.com:SmartlyAI/hestia.git
-  cd hestia && git checkout snips && cd ..
-else
-  echo 'Updating hestia repository'
-  cd hestia &&  git fetch && git checkout snips && git pull origin snips && cd ..
-fi
-
-# Cloning mars only if it does not exist
-if [ ! -d "mars" ]; then
-  git clone git@github.com:SmartlyAI/mars.git
-  cd mars && git checkout $1 && cd ..
-else
-  echo 'Updating mars repository'
-  cd mars &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
-fi
-
-# Cloning snips-nlu-parse only if it does not exist
-if [ ! -d "snips-nlu-parse" ]; then
-  git clone git@github.com:SmartlyAI/snips-nlu-parse.git
-  cd snips-nlu-parse && git checkout $1 && cd ..
-else
-  echo 'Updating snips-nlu-parse repository'
-  cd snips-nlu-parse &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
-fi
-
-# Cloning snips-nlu-train only if it does not exist
-if [ ! -d "snips-nlu-train" ]; then
- git clone git@github.com:SmartlyAI/snips-nlu-train.git
- cd snips-nlu-train && git checkout $1 && cd ..
-else
-  echo 'Updating snips-nlu-train repository'
-  cd snips-nlu-train &&  git fetch && git checkout $1 && git pull origin $1 && cd ..
-fi
-
-# Build stack
-privatekey=$(cat ~/.ssh/id_rsa)
-publickey=$(cat ~/.ssh/id_rsa.pub)
-SSH_PRIVATE_KEY="$privatekey" SSH_PUBLIC_KEY="$publickey" docker-compose build
-
-# Push and launch snips_nlu stack
-docker-compose push && env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml snipsnlu
+# Launch the stack named snipsnlu
+env $(cat .env | grep ^[A-Z] | xargs) docker stack deploy --compose-file docker-compose.yml snipsnlu
