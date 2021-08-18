33d2df79f5e57e1f6a077141f5b62c50256c9ec7
Elvis MBONING <elvis.mboningtchiaze@orange.com>
2021-08-18 15:16:08 +0200

    update max_utterance for training

--- snips_nlu/intent_classifier/log_reg_classifier.py
+++ snips_nlu/intent_classifier/log_reg_classifier.py
@@ -31,7 +31,7 @@ logger = logging.getLogger(__name__)
 LOG_REG_ARGS = {
     "loss": "log",
     "penalty": "l2",
-    "max_iter": 1000,
+    "max_iter": 10000,
     "tol": 1e-3,
     "n_jobs": -1
 }
