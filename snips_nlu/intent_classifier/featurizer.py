0ef581a923dfcceb4b6acd7eb81eb626d29624c6
reda-g <97886664+reda-g@users.noreply.github.com>
2022-11-03 18:00:39 +0100

    add breakpoint

--- snips_nlu/intent_classifier/featurizer.py
+++ snips_nlu/intent_classifier/featurizer.py
@@ -135,6 +135,7 @@ class Featurizer(ProcessingUnit):
         # and fit(x, y).transform(x)
         return self.tfidf_vectorizer.transform(x)
 
+    import pdb; pdb.breakpoint()
     def _fit_cooccurrence_vectorizer(self, x, classes, none_class, dataset):
         import numpy as np
         from sklearn.feature_selection import chi2
